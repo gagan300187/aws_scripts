5fd81912f8e9b49417f84abf399e681986e7066f
gagan300187 <35688654+gagan300187@users.noreply.github.com>
2022-05-18 13:49:25 +0530

    modules descriptions

--- aws_alarms_excel.py
+++ aws_alarms_excel.py
@@ -3,17 +3,38 @@ import json
 import boto3
 
 def get_config_data():
+    """Fetches user defined configuration data from 'config.json' file
+
+    Returns:
+        dict: user defined config data in dict format.
+    """
     cfile = open('config.json','r')
     cdata = json.load(cfile)
     return cdata 
 
 def fetch_alarms_data():
+    """fetches cloudwatch alarms data from configured AWS account
+
+    Returns:
+        dict: Cloudwatch data in form of dict.
+    """
     client = boto3.client('cloudwatch')
     data = client.describe_alarms()
     return data
 
 
 def filter_namespace(filter_data, ns_data):
+    """seggregates alarms data as per namespace given in config.json
+    this namespace mapping will be used to categorize the type of resource/cloudwatch alarm
+    if namespace is not provided in config.json file, then data will be allocated to generic/others category.
+
+    Args:
+        filter_data (list): list of all cloudwatch alarms received from aws
+        ns_data (dict): namespace and settings mapping of configured namespaces
+
+    Returns:
+        dict: dictionary of seggregated namespace with cloudwatch alarms details
+    """
     for i in filter_data:
         namespace = i[4]
         if namespace in ns_data.keys():
@@ -25,6 +46,13 @@ def filter_namespace(filter_data, ns_data):
 
 
 def generate_excel(excel_filename, sheet_data, sheet_columns):
+    """Geerates excel file with multiple sheets (with sheet_name configured in namespace data)
+
+    Args:
+        excel_filename (string): name of the output excel file that will store the result
+        sheet_data (dict): Cloudwatch alarms data to be saved in given sheets
+        sheet_columns (list): list of selected columns to be saves in sheet
+    """
     writer=pd.ExcelWriter(excel_filename)
     for sheet in sheet_data:
         sheet_name=sheet_data[sheet]['name'] #define sheet name for excel report
